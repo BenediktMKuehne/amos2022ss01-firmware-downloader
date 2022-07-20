f362a9b4d21f295383a2b306ed3abcef9136c5ee
TEJESH PALA <104498119+TejeshPala@users.noreply.github.com>
2022-07-21 01:25:19 +0200

    correction:unittesting.yml(run_tests.sh)
    Signed-off-by: TEJESH PALA <104498119+TejeshPala@users.noreply.github.com>

--- run_tests.sh
+++ run_tests.sh
@@ -9,4 +9,5 @@ cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd || exit 1
 cd .. || exit 1
 
 # run all unit tests
+python -c "import os, sys; sys.path.append(os.path.abspath(os.path.join('./amos2022ss01-firmware-downloader/', '')))"
 python -m pytest --import-mode=append ./amos2022ss01-firmware-downloader/unit_tests/*
