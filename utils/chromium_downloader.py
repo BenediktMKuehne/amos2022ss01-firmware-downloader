f490b855f178bad63ec1526a690a4372e0658534
TEJESH PALA <104498119+TejeshPala@users.noreply.github.com>
2022-07-24 14:13:18 +0200

    correction: linting, chromium_downloader
    Signed-off-by: TEJESH PALA <104498119+TejeshPala@users.noreply.github.com>

--- utils/chromium_downloader.py
+++ utils/chromium_downloader.py
@@ -1,9 +1,9 @@
 import os
 import zipfile
 import sys
+import inspect
 import requests
 import wget
-import inspect
 current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir)
