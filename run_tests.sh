a4fd5c49c9ab8b94f58e20c556f7fc67c9c99f92
TEJESH PALA <104498119+TejeshPala@users.noreply.github.com>
2022-07-25 22:55:23 +0200

    corrections: run_tests.sh
    Signed-off-by: TEJESH PALA <104498119+TejeshPala@users.noreply.github.com>

--- run_tests.sh
+++ run_tests.sh
@@ -23,5 +23,5 @@ cd ../amos2022ss01-firmware-downloader || exit 1
 # run all unit tests
 python -c "import os, sys; sys.path.append(os.path.abspath(os.path.join('./amos2022ss01-firmware-downloader/', '')))"
 python -c "from utils.chromium_downloader import ChromiumDownloader; ChromiumDownloader().executor()"
-chmod 755 chromedriver.exe
+sudo chmod 755 ~/chromedriver.exe
 python -m pytest --import-mode=append unit_tests/*
