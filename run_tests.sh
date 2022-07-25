744eb6cf267647bfbfa7700b021fefd248fad969
TEJESH PALA <104498119+TejeshPala@users.noreply.github.com>
2022-07-25 23:12:00 +0200

    corrections: run_tests.sh
    Signed-off-by: TEJESH PALA <104498119+TejeshPala@users.noreply.github.com>

--- run_tests.sh
+++ run_tests.sh
@@ -6,9 +6,9 @@ sudo apt-get install unzip
 wget https://chromedriver.storage.googleapis.com/103.0.5060.134/chromedriver_linux64.zip
 
 unzip chromedriver_linux64.zip
-sudo mv chromedriver /usr/local/bin
-sudo chown root:root /usr/local/bin/chromedriver
-sudo chmod +x /usr/local/bin/chromedriver
+sudo mv chromedriver /home/runner/work/amos2022ss01-firmware-downloader/amos2022ss01-firmware-downloader/utils/
+sudo chown root:root /home/runner/work/amos2022ss01-firmware-downloader/amos2022ss01-firmware-downloader/utils/chromedriver
+sudo chmod +x /home/runner/work/amos2022ss01-firmware-downloader/amos2022ss01-firmware-downloader/utils/chromedriver
 
 # Export paths
 export PYTHONPATH="$PYTHONPATH: ../amos2022ss01-firmware-downloader"
@@ -22,6 +22,6 @@ cd ../amos2022ss01-firmware-downloader || exit 1
 
 # run all unit tests
 python -c "import os, sys; sys.path.append(os.path.abspath(os.path.join('./amos2022ss01-firmware-downloader/', '')))"
-python -c "from utils.chromium_downloader import ChromiumDownloader; ChromiumDownloader().executor()"
-sudo chmod +rwx /home/runner/work/amos2022ss01-firmware-downloader/amos2022ss01-firmware-downloader/utils/chromedriver.exe
+#python -c "from utils.chromium_downloader import ChromiumDownloader; ChromiumDownloader().executor()"
+#sudo chmod +rwx find -name "chromedriver.exe"
 python -m pytest --import-mode=append unit_tests/*
