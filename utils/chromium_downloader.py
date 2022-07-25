6842940cdeac20bb1949a30b2644c8f59a7bcab1
TEJESH PALA <104498119+TejeshPala@users.noreply.github.com>
2022-07-25 22:30:18 +0200

    corrections: chromium_downloader path
    Signed-off-by: TEJESH PALA <104498119+TejeshPala@users.noreply.github.com>

--- utils/chromium_downloader.py
+++ utils/chromium_downloader.py
@@ -24,19 +24,19 @@ class ChromiumDownloader:
         download_url = "https://chromedriver.storage.googleapis.com/{}/chromedriver_win32.zip".format(response)
         print(parent_dir)
         print(response, download_url)
-        chromium_zip = wget.download(download_url, fr'{parent_dir}\utils\chromedriver.zip')
-        print(fr'{parent_dir}\utils\chromedriver.zip' if os.path.isfile(fr'{parent_dir}\utils\chromedriver.zip')
-              else None)
+        chromium_zip = wget.download(download_url, fr'{parent_dir}\utils\chromedriver.zip'.replace('\\', '/'))
+        print(fr'{parent_dir}\utils\chromedriver.zip'.replace('\\', '/') if os.path.isfile(fr'{parent_dir}\utils\
+                                                                        chromedriver.zip'.replace('\\', '/'))else None)
         with zipfile.ZipFile(chromium_zip, 'r') as zip_ref:
-            zip_ref.extractall(fr'{parent_dir}\utils')
-        if os.path.isfile(fr'{parent_dir}\utils\chromedriver.exe'):
-            print(fr'file found and is at {parent_dir}\utils\chromedriver.exe')
+            zip_ref.extractall(fr'{parent_dir}\utils'.replace('\\', '/'))
+        if os.path.isfile(fr'{parent_dir}\utils\chromedriver.exe'.replace('\\', '/')):
+            print(fr'file found and is at {parent_dir}\utils\chromedriver.exe'.replace('\\', '/'))
         os.remove(chromium_zip)
 
     def executor(self):
         # Checksum for chromedriver
-        if os.path.exists(fr'{parent_dir}\utils\chromedriver.exe'):
-            os.remove(fr'{parent_dir}\utils\chromedriver.exe')
-        if "chromedriver.exe" not in os.listdir(fr'{parent_dir}\utils'):
+        if os.path.exists(fr'{parent_dir}\utils\chromedriver.exe'.replace('\\', '/')):
+            os.remove(fr'{parent_dir}\utils\chromedriver.exe'.replace('\\', '/'))
+        if "chromedriver.exe" not in os.listdir(fr'{parent_dir}\utils'.replace('\\', '/')):
             print("chromedriver.exe is not present in local path, so installing chromedriver")
             self.load_and_extract()
